4827ae03cd43ec2eb5516be161f29bab10802211
Marwane <marwane.seffah@etu.univ-amu.fr>
2026-03-28 20:00:28 +0100

    ajout de mes fichiers v1

---
+++ periodicites_par_intervalles.py
@@ -0,0 +1,23 @@
+import pandas 
+import matplotlib.pyplot as plt
+
+def periodicites_par_intervalles():
+    """
+    Affiche un diagramme des périodicités par intervalles
+    selon si ces periodicités sont comprises entre 1 jour et 1 mois, 
+    1 mois et 3 mois, 3 mois et 6 mois, 6 mois et 1 ans ou bien 1 an et 2 ans.
+    On ne prend en compte que les machines dont la periodicité d'entretien est 
+    inférieure ou égale à 2 ans (730 jours).
+    pre:
+    post:
+    """
+    types_entretien = pandas.read_csv("typeEntretien.csv")
+    plt.hist(types_entretien["periodicite"],
+             range=[1,730], # Entretiens effectués de tout les jours à tout les 2 ans
+             bins=[1,30,90,180,365,730], # en groupant par fréquences différentes
+             color="orange",
+             edgecolor='black',
+             align="left")
+    plt.show()
+
+periodicites_par_intervalles()
