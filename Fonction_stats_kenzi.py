359b38c39aa86f887f861c405f180fcbb8d54237
Kenzi <kenziyacini@gmail.com>
2026-03-29 16:15:35 +0200

    Fix nom du csv

--- Fonction_stats_kenzi.py
+++ Fonction_stats_kenzi.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 def repartition_dignite():
 
-	Tenrac = pd.read_csv("tenracinsh.csv")
+	Tenrac = pd.read_csv("Tenrac.csv")
 	Tenrac['nomDignite'] = Tenrac['nomDignite'].fillna('Sans Dignité') #juste pour remplacer les NULL par "Sans Dignité"
 
 	groupby = Tenrac.groupby(['nomTitre', 'nomDignite']).size().unstack(fill_value=0) #groupby pour regrouper par titre et par dignite , le .size pour savoir combien de Tenracs ont chaque combinaisons et le .unstack pour 																			transformer le groupby en grille (en prenant les lignes de nomDignite pour les changer en colonnes) ce qui rendra la figure + lisible , le 																			fill_value sert à éviter les NaN
