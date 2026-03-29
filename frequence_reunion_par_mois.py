6a34057e7ca6033c3631abc60ef8924713338945
marwane__s <marwane.seffah@etu.univ-amu.fr>
2026-03-29 16:35:32 +0200

    Changement nom 2

--- frequence_reunion_par_mois.py
+++ frequence_reunion_par_mois.py
@@ -5,11 +5,10 @@ def frequence_reunion_par_mois ():
     """
     affiche un diagramme du nombre de réunions par mois de l'année
     """
-    reunion  = pd.read_csv("reunion.csv")
+    reunion  = pd.read_csv("Reunion.csv")
     reunion["moisReunion"] = pd.to_datetime(reunion["dateReunion"]).dt.month # https://pandas.pydata.org/docs/user_guide/timeseries.html#converting-to-timestamps
     # https://pandas.pydata.org/docs/user_guide/groupby.html#aggregation
     stats_reunion = reunion.groupby('moisReunion', as_index=False).count()
-    print(stats_reunion[["moisReunion", "idReunion"]])
     
     plt.bar(stats_reunion["moisReunion"], stats_reunion["idReunion"],
              color="#6BF178",
